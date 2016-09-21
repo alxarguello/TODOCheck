07aba36b43444f5cd4c42b6b122d21b3d227f073
Alejandro Arguello <alx.arguello@gmail.com>
2016-09-21 13:38:02 -0700

    Initial setup

---
+++ mobile/src/main/java/com/alxarguello/apps/todocheck/MainActvity.java
@@ -0,0 +1,13 @@
+package com.alxarguello.apps.todocheck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
